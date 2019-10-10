d6433790b77bae6eea6ef947fdc943363ff028ac
victortiads <baappaap123@hotmail.com>
2019-10-10 14:23:40 -0300

    start working on details activity

---
+++ app/src/main/java/com/example/desafioconcrete/view/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.desafioconcrete.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desafioconcrete.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
